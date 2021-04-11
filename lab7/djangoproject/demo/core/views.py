53d63ebc947257e697dd02ba9a126da7724ac23c
Sens3ii <densteeper@gmail.com>
2021-04-11 08:48:29 +0600

    updated

---
+++ lab7/djangoproject/demo/core/views.py
@@ -0,0 +1,18 @@
+from django.http.response import HttpResponse, JsonResponse
+from core.models import Product
+
+
+def products_list(request):
+    # SELECT * FROM core_product;
+    products = Product.objects.all()
+    products_json = [product.to_json() for product in products]
+    return JsonResponse(products_json, safe=0)
+
+
+def product_detail(request, product_id):
+    # SELECT * FROM core_products WHERE id = product_id;
+    try:
+        product = Product.objects.get(id=product_id)
+    except Product.DoesNotExist as e:
+        return JsonResponse({'message': str(e)})
+    return JsonResponse(product.to_json())
---
+++ lab7/djangoproject/py36venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\itforyou\webdev\lab7\djangoproject\py36venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

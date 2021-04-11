84041f01659c155748c96a6f62b6714a7359439d
Sens3ii <densteeper@gmail.com>
2021-04-11 16:05:25 +0600

    'lecture_example'

---
+++ lab9/demo/core/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from core.models import Product
+from api.models import Category
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'description', 'category')
+    search_fields = ('name', 'price',)
+    list_filter = ('category',)
+
+
+admin.site.register(Category)
